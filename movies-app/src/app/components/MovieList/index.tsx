5d31b8a44fe658aa4da583a80635ca9f577fd42b
Eduardo silva <86812584+Eduardo09e32y4rhf@users.noreply.github.com>
2025-02-01 12:28:20 -0300

    atualizado

--- movies-app/src/app/components/MovieList/index.tsx
+++ movies-app/src/app/components/MovieList/index.tsx
@@ -1,37 +1,64 @@
 
 
 'use client';
-import { useState } from 'react';
-// movies.ts
-import axios from 'axios';
-
+import { useEffect, useState } from 'react';
 
 
 import './index.scss';
+import { url } from 'inspector';
+import axios from 'axios';
+export interface MovieType{
+    title: string;
+    poster_path:string;
+    overview:string;
+    vote_average:number;
+}
 
 export default function MovieList (){
-    
-const TMDB_API_KEY = '174ac822eacf29a95798c149ea01c241';
+    const [movies, setMovies] = useState([]);
+    useEffect(()=>{
+        getMovies();
+    },[];)
 
-export const getMoviesFromDatabase = async () => {
-  const response = await axios.get(`https://api.themoviedb.org/3/discover/movie{174ac822eacf29a95798c149ea01c241}`);
-  return response.data.results;
-};
-            
-            params: {
+const getMovies = ()  =>{
+    axios({
+        method: 'get',
+        url:'https://api.themoviedb.org/3/discover/movie{174ac822eacf29a95798c149ea01c241',
+        params:{
+            api_key:'174ac822eacf29a95798c149ea01c241',
+            languge:'pt-br'
+        }
 
-                language: 'pt-br'
-            }
-        }).then( 'response' =>{
-            console.log(response);
-        })
-    }
-
-    getMovies();
-    return (
-
-        <ul className="movie-list">
-            <li></li>
-        </ul>
-    );
+    }) .then( response=> {
+       setMovies(response.data.results);
+       
+    })
 }
+
+
+return(
+    <ul className='movie-list'>
+
+        {movies.map((movie)=>  
+          <li className='movie-card'>
+           
+       <p> 
+        {movie.title}
+        </p>    
+        <p  className='description'>
+            {movie.overview}
+        </p>
+        <img src="{'https://image.tmdb.org/t/p/original${movie.poster_path}'}" alt="" />
+        <p>
+            {movie.vote_average}
+        </p>
+          </li>
+        )}
+       
+    </ul>
+)
+
+}
+    
+
+
